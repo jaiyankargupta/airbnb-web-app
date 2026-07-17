6a9929b31aa24e3c73530933f26893d9b39a4345
rustyn <jaiyankargupta@gmail.com>
2026-07-17 21:16:50 +0530

    integrate Turso cloud database connection using sqlalchemy-libsql

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,12 +1,33 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./airbnb.db"
+load_dotenv()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+db_url = os.getenv("TURSO_DATABASE_URL", "sqlite:///./airbnb.db")
+connect_args = {}
+
+if db_url.startswith("libsql://") or db_url.startswith("https://"):
+    base = db_url.replace("libsql://", "").replace("https://", "")
+    sqlalchemy_url = f"sqlite+libsql://{base}"
+    if "?" in base:
+        if "secure=true" not in base:
+            sqlalchemy_url += "&secure=true"
+    else:
+        sqlalchemy_url += "?secure=true"
+        
+    auth_token = os.getenv("TURSO_AUTH_TOKEN")
+    if auth_token:
+        connect_args["auth_token"] = auth_token
+elif db_url.startswith("sqlite"):
+    sqlalchemy_url = db_url
+    connect_args = {"check_same_thread": False}
+else:
+    sqlalchemy_url = db_url
+
+engine = create_engine(sqlalchemy_url, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
